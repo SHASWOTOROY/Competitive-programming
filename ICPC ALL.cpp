b5053341f31d14b64ad111dd70556799f76e7ff0
Shaswata Roy <63513936+SHASWOTOROY@users.noreply.github.com>
2023-11-03 02:10:25 +0600

    Update ICPC ALL.cpp

--- ICPC ALL.cpp
+++ ICPC ALL.cpp
@@ -2366,3 +2366,135 @@ int main()
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+
+#include<bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+typedef long long ll;
+typedef tree<pair<ll,ll>, null_type, less<pair<ll,ll>>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef tree<ll, null_type, greater<ll>, rb_tree_tag, tree_order_statistics_node_update> pbdsx;
+typedef tree<ll, null_type, less<ll>, rb_tree_tag, tree_order_statistics_node_update> pbds1;// find_by_order, order_of_key, less, greater,less_equal
+typedef tree<ll, null_type, less_equal<ll>, rb_tree_tag, tree_order_statistics_node_update> ordered_multiset;
+
+typedef vector<ll> vec;
+typedef vector<string> vecs;
+typedef pair<ll, ll> pll;
+typedef vector<pair<int, int>> vp;
+typedef set<ll> st;
+typedef set<pair<ll, ll>> sp;
+typedef multiset<ll> mst;
+typedef multiset<pair<ll, ll>> mstp;
+typedef map<ll, ll> mp;
+typedef map<char, ll> mpc;
+const ll mx=1e6+1;
+bitset<mx>isprime;
+#define fastio  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+vec vv1[mx];
+bool visited[mx];
+ll cnt=0;
+void dfs(ll u){
+  visited[u]=1;
+  cnt++;
+  for(auto x: vv1[u]){
+     if(visited[x]==0){
+        dfs(x);
+     }
+  }
+  return;
+
+}
+int main()
+{
+    //fastio;
+    ll n,e;
+    cin>>n;
+    e=n-1;
+    for(ll i=1;i<=e;i++){
+        ll u,v;
+        cin>>u>>v;
+        vv1[u].push_back(v);
+        vv1[v].push_back(u);
+    }
+    ll ans=0;
+    for(ll i=1;i<=n;i++){
+        if(visited[i]==0){
+            cnt=0;
+            dfs(i);
+            ans=max(ans,cnt);
+        }
+    }
+    cout<<" Maximum Size of Connected component of a graph "<<ans<<endl;
+
+
+}
+
+#include<bits/stdc++.h>
+
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+
+using namespace std;
+using namespace __gnu_pbds;
+
+typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds;
+typedef long long ll;
+typedef vector<ll> vec;
+typedef vector<string> vecs;
+typedef pair<ll, ll> pll;
+typedef vector<pair<int, int>> vp;
+typedef set<ll> st;
+typedef set<pair<ll, ll>> sp;
+typedef multiset<ll> mst;
+typedef multiset<pair<ll, ll>> mstp;
+typedef map<ll, ll> mp;
+typedef map<char, ll> mpc;
+const ll mx=1e8+1;
+bitset<mx>isprime;
+#define fastio  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+
+int main()
+{
+    //fastio;
+    unordered_map<char,ll>bracket= {{'(',-1},{'{',-2},{'[',-3},{')',1},{'}',2},{']',3}};
+    string s;
+    stack<char>st;
+    cin>>s;
+    for(auto x:s)
+    {
+        if(bracket[x]<0)
+        {
+            st.push(x);
+        }
+        else
+        {
+            if(st.empty())
+            {
+                cout<<"NO"<<endl;
+                return 0;
+            }
+            else
+            {
+                char xx=st.top();
+                st.pop();
+                if(bracket[xx]+bracket[x]!=0)
+                {
+                    cout<<"NO"<<endl;
+                    return 0;
+                }
+            }
+        }
+    }
+    cout<<"YES"<<endl;
+    main();
+
+    //main();
+
+}
+
+
+
+
